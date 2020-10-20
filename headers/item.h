890ee293a0a6c3ec9585547529cee81a7f0fcc00
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-10-21 10:46:02 +1100

    Put public at start of struct

--- headers/item.h
+++ headers/item.h
@@ -1,8 +1,9 @@
 struct item
 {
+public:
   unsigned int id;
   char name[16];
   char displayname[16];
 
-  public: item(unsigned int id, char name[16], char displayname[16]);
+  item(unsigned int id, char name[16], char displayname[16]);
 };
