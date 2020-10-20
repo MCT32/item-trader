86533b4a48202aceb0b970f6f3c778c2400740ee
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-10-20 17:57:47 +1100

    Added item structure and first item, toilet paper

---
+++ headers/items.h
@@ -0,0 +1,10 @@
+struct item
+{
+  unsigned int id;
+  char[] namespace;
+  char[] displayname;
+};
+
+item toilet_paper = {0,
+                     "toilet_paper",
+                     "Toilet Paper"};
