73e37bddab8e214f9471f969e2ff6be660fd7785
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-10-20 18:44:41 +1100

    fixed stashed changes

--- headers/items.h
+++ headers/items.h
@@ -8,25 +8,10 @@ struct item
 };
 
 // All items
-<<<<<<< Updated upstream
 const item toilet_paper = {
   0,
   "toilet_paper",
   "Toilet Paper"
-=======
-const item empty = {
-  0,
-  "empty",
-  "Empty",
-  "Literally nothing."
-};
-
-const item toilet_paper = {
-  1,
-  "toilet_paper",
-  "Toilet Paper",
-  "Average toilet paper roll with each of the 1,000 2 ply sheets having a size of 3.7in(9.4cm) x 4.1in(10cm)."
->>>>>>> Stashed changes
 };
 
 // Slot structure
@@ -40,11 +25,5 @@ struct item_slot
 // Inventory structure
 struct inventory
 {
-<<<<<<< Updated upstream
-  
-=======
-  unsigned int width;
-  unsigned int height;
-  item_slot slot[][];
->>>>>>> Stashed changes
+
 };
