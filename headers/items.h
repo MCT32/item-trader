cbe2310ba9da0aa141676a526cf9924ea5f1a49f
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-10-20 18:37:50 +1100

    Added "empty" item, item definitions and slot and inventory structures.

--- headers/items.h
+++ headers/items.h
@@ -1,3 +1,4 @@
+// Item structure
 struct item
 {
   unsigned int id;
@@ -5,6 +6,23 @@ struct item
   char[] displayname;
 };
 
-item toilet_paper = {0,
-                     "toilet_paper",
-                     "Toilet Paper"};
+// All items
+const item toilet_paper = {
+  0,
+  "toilet_paper",
+  "Toilet Paper"
+};
+
+// Slot structure
+struct item_slot
+{
+  item* item;
+  unsigned int quantity;
+  unsigned int max;
+};
+
+// Inventory structure
+struct inventory
+{
+  
+};
