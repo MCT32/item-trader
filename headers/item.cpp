ac4977aa46313677aa54241667684ae6daad99a4
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-10-21 10:03:41 +1100

    Moved item struct constructor

--- headers/item.cpp
+++ headers/item.cpp
@@ -1,1 +1,8 @@
 #include "item.h"
+
+item::item(unsigned int id_, char name_[16], char displayname_[16])
+{
+  id = id_;
+  name = name_;
+  displayname = displayname_
+};
--- headers/item.h
+++ headers/item.h
@@ -3,9 +3,4 @@ struct item
   unsigned int id;
   char name[16];
   char displayname[16];
-
-  item(unsigned int id_, char name_[16], char displayname_[16])
-  {
-
-  };
 };
