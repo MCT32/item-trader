d8266d983be8f738d14c10e311e2a75de243e035
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-10-21 10:48:36 +1100

    added item structure description and constructor for it

--- headers/item.cpp
+++ headers/item.cpp
@@ -1,9 +1,17 @@
 #include "item.h"
 #include <cstring>
 
-item::item(unsigned int id, char name[16], char displayname[16])
+item::item(unsigned int id, char name[], char displayname[])
 {
   this->id = id;
   memcpy(this->name, name, 16);
   memcpy(this->displayname, displayname, 16);
+}
+
+item::item(unsigned int id, char name[], char displayname[], char description[])
+{
+  this->id = id;
+  memcpy(this->name, name, 16);
+  memcpy(this->displayname, displayname, 16);
+  memcpy(this->description, description, 250);
 }
--- headers/item.h
+++ headers/item.h
@@ -4,6 +4,8 @@ public:
   unsigned int id;
   char name[16];
   char displayname[16];
+  char description[250];
 
-  item(unsigned int id, char name[16], char displayname[16]);
+  item(unsigned int id, char name[], char displayname[]);
+  item(unsigned int id, char name[], char displayname[], char description[]);
 };
