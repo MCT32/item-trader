2fda3a2af23bb9d9960fc714ea65a8b089d2c7b0
MCT32 <32090502+MCT32@users.noreply.github.com>
2020-10-21 11:54:10 +1100

    redoing inventory

--- headers/inventory.cpp
+++ headers/inventory.cpp
@@ -1,18 +1,3 @@
 #include "inventory.h"
 
 /* Constructors */
-inventory::inventory()
-{
-
-};
-
-inventory::inventory(unsigned int max)
-{
-  for(int i; i < 4; i++)
-  {
-    for(int j; j < 5; j++)
-    {
-      slots[i][j] = new slot();
-    }
-  }
-};
--- headers/inventory.h
+++ headers/inventory.h
@@ -5,9 +5,7 @@ struct inventory
 {
 public:
   /* Variables */
-  slot slots[4][5];
+  
 
   /* Constructors */
-  inventory();
-  inventory(unsigned int max);
 }
